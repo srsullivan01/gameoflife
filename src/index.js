7749ffc9b53bb48b9ac0ed1abd8bd428dc4815c1
Rory <srsullivan01@gmail.com>
2017-11-13 16:24:15 -0500

    built functions to seed board

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Box extends React.Component {
+  selectBox= () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  }
+  render() {
+    return(
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
+
 class Grid extends React.Component {
   render(){
-    const width = this.props.cols * 14;
+    const width = (this.props.cols * 16) + 1;
     var rowsArr = [];
 
     var boxClass = "";
-    for (var i = 0; i < this.props.rows; i++){
-      for (var j = 0; j < this.props.cols; j++){
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
         let boxId = i + "_" + j;
 
         boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
@@ -22,15 +37,15 @@ class Grid extends React.Component {
             col={j}
             selectBox={this.props.selectBox}
           />
-        )
+        );
       }
     }
 
     return(
       <div className="grid" style={{width: width}}>
-        {{rowsArr}}
+        {rowsArr}
       </div>
-    )
+    );
   }
 }
 
@@ -46,6 +61,38 @@ constructor(){
     gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
   }
 }
+
+selectBox = (row, col) => {
+  let gridCopy = arrayClone(this.state.gridFull);
+  gridCopy[row][col] = !gridCopy[row][col];
+  this.setState({
+    gridFull: gridCopy
+  });
+}
+
+seed = () => {
+  let gridCopy = arrayClone(this.state.gridFull);
+  for (let i = 0; i < this.rows; i++) {
+    for (let j = 0; j < this.cols; j++) {
+      if (Math.floor(Math.random() * 4) === 1) {
+        gridCopy[i][j] = true;
+      }
+    }
+  }
+  this.setState({
+    gridFull: gridCopy
+  });
+}
+
+playButton = () => {
+  clearInterval(this.intervalId)
+  this.intervalId = setIntervcal(this.play, this.speed);
+}
+
+componentDidMount() {
+  this.seed();
+}
+
   render() {
     return (
       <div>
@@ -60,5 +107,9 @@ constructor(){
       </div>
     )
   }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 ReactDOM.render(<Main />, document.getElementById('root'));
